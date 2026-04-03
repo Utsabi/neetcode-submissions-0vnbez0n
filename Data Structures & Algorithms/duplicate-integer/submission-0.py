9e6e49424ec97f1cc58e33d38493dc08ba69dacb
Utsabi Dangol <utsabidangol123@gmail.com>
2026-04-03 09:04:28 -0500

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        set_num = set(nums)
+        if len(set_num)== len(nums):
+            return False
+        else:
+            return True
+        
