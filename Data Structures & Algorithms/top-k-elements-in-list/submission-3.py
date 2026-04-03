cd2757603b8a8688245f0f45d1046ca1b346153e
Utsabi Dangol <utsabidangol123@gmail.com>
2026-04-03 15:40:06 -0500

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        map={}
+        for item in nums:
+            map[item] = map.get(item, 0) + 1
+        print(map)
+        result = sorted(map, key=lambda x:map[x], reverse=True)[:k]
+        return result
+        
