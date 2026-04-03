77bb9e423db3cae981783c0b27fd04418607fd3e
Utsabi Dangol <utsabidangol123@gmail.com>
2026-04-03 14:53:22 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list) 
+        for word in strs:
+            word_sort = "".join(sorted(word))
+            result[word_sort].append(word)
+        
+        return list(result.values())
+            
