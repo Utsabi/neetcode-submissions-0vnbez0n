91367a7e94485a87b66431403fdab9a8607420cf
Utsabi Dangol <utsabidangol123@gmail.com>
2026-04-05 19:41:53 -0500

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        word = ''.join(c for c in s.lower() if c.isalpha() or c.isnumeric())
+        print(word)
+
+        st, end = 0, len(word)-1
+
+        while st <= end:
+            if word[st]!= word[end]:
+                print(word[st],word[end])
+                return False
+            else:
+                st +=1
+                end -=1
+            
+        return True
