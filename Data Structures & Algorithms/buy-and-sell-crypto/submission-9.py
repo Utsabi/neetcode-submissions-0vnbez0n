7535c0bd2588bc45cfee50a838f8c9351656d73c
Utsabi Dangol <utsabidangol123@gmail.com>
2026-04-07 17:02:55 -0500

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        i, j = 0,1
+        trans=0
+
+        while j < len(prices):
+            if prices[i]<prices[j]:
+                trans=max(trans,prices[j]-prices[i])               
+            else:               
+                i=j
+                
+            j=j+1   
+        return trans
+
+        
